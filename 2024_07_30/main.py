118d4198a9b2995d2cd89b6349243226c34e48d3
roberthsu2003 <roberthsu2003@gmail.com>
2024-07-30 21:08:17 +0800

    modify

--- 2024_07_30/main.py
+++ 2024_07_30/main.py
@@ -1,10 +1,4 @@
-#自訂的function
-def underage(a,n):
-    if a < 18:
-        print(f'{n}:未成年')
-    else:
-        print(f'{n}:成年')
-
+from widget.tools1 import underage
 
 name = input("請輸入姓名:")
 age = int(input("your age:"))
---
+++ 2024_07_30/widget/tools1.py
@@ -0,0 +1,6 @@
+#自訂的function
+def underage(a,n):
+    if a < 18:
+        print(f'{n}:未成年')
+    else:
+        print(f'{n}:成年')
